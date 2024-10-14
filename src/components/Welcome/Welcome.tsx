0a2cc0e1f271bebdded608e8539b323afaec9260
MohammadAli Pishva <m.a.pishva110@gmail.com>
2024-10-14 16:43:21 +0330

    Impementing welcome component

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,21 @@
+import "./_welcome.scss";
+
+const Welcome = () => {
+  return (
+    <div className="welcome">
+      <p className="welcome-to">welcome to Barista.co</p>
+      <h1 className="cafe-name">Cafe Klang</h1>
+      <p className="text">your facvourite coffee daily lives.</p>
+      <div>
+        <button type="button" className="btn btn-outline-light">
+          Our Story
+        </button>
+        <button type="button" className="btn btn-primary">
+          Check Menu
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
